476b5e11eb58b6627f4f3f79bbb37e03a07889a9
Sachmilan <sachmilansingh@gmail.com>
2022-07-26 12:44:54 -0600

    pushing the updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,10 @@ const humidityElement = $("#humidity");
 const windSpeedElement = $("#wind-speed");
 const indexUVElement = $("#UV-index");
 const futureForecasteElement = $("#five-day-forecast-boxes");
+
+
+// Linkin the buttons to the javascript
+const searchButton = $("#search-button");
+const clearButton = $("#clear-history-button");
+// my personal api key
+const apiKey = "1291db61f641ff72ce7519899878bf5a";
